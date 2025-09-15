acc0b598377dd7c12bb26470b812cee61ae951b2
Jhon-Crow <jhamish@mail.ru>
2025-09-15 18:30:42 +0300

    finished pet-list

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,10 @@ const nextConfig: NextConfig = {
           {
               protocol: "https",
               hostname: "bytegrad.com"
+          },
+          {
+              protocol: "https",
+              hostname: "images.unsplash.com"
           }
       ]
   }
--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,23 +1,35 @@
 import Image from "next/image";
 
-export default function PetList() {
+type Pet = {
+    id: string;
+    name: string;
+    ownerName: string;
+    imageUrl: string;
+    age: number;
+    notes: string;
+};
+
+export default function PetList({pets}: { pets: Pet[] }) {
     return (
         <ul className="bg-white border-b border-black/[0.08]">
-            <li>
-                <button
-                    className="flex items-center h-[70px]
-                    w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2]
-                    focus:bg-[#EFF1F2] transition">
-                    <Image
-                        src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
-                        alt="Pet image"
-                        width={45}
-                        height={45}
-                        className="rounded-full object-cover"
-                    />
-                    <p className="font-semibold">Benjamin</p>
-                </button>
-            </li>
+            {pets.map((pet) => (
+                <li key={pet.id}>
+                    <button
+                        className="flex items-center h-[70px]
+                        w-full cursor-pointer px-5
+                        text-base gap-3 hover:bg-[#EFF1F2]
+                        focus:bg-[#EFF1F2] transition">
+                        <Image
+                            src={pet.imageUrl}
+                            alt="Pet image"
+                            width={45}
+                            height={45}
+                            className="rounded-full w-[45px] h-[45px] object-cover"
+                        />
+                        <p className="font-semibold">{pet.name}</p>
+                    </button>
+                </li>
+            ))}
         </ul>
     );
 }
