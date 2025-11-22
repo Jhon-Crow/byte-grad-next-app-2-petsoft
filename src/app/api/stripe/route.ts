26a4626a0d7dd7575c750a57ec4e5eaee5a86ecc
Jhon-Crow <jhamish@mail.ru>
2025-11-22 12:19:45 +0300

    add api stripe route (for success charge case)

---
+++ src/app/api/stripe/route.ts
@@ -0,0 +1,18 @@
+import {prisma} from "@/lib/db";
+
+export async function POST(request: Request) {
+    const data = await request.json();
+
+    console.log(data)
+    await prisma.user.update({
+        where: {
+            email: data.data.object.billing_details.email
+        },
+        data: {
+            hasAccess: true
+        }
+    })
+
+
+    return Response.json(null, {status: 200});
+}
